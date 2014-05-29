f3f1a4e0c264e2b893c9dbb2f804957f3455951b
YU <yulu8798@gmail.com>
2014-05-29 17:33:34 +0800

    start

---
+++ gen/com/littlecheesecake/grids/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.littlecheesecake.grids;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/littlecheesecake/grids/views/GridsView.java
@@ -0,0 +1,20 @@
+package com.littlecheesecake.grids.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+
+public class GridsView  extends View{
+
+	private Bitmap mBitmap; //background canvas for drawing
+	
+	public GridsView(Context context){
+		super(context);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh){
+		this.
+	}
+	
+}
